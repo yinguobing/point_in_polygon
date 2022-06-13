2fc28ed2b68acdc98f97393a1072afadf3e06416
yinguobing <yinguobing@gmail.com>
2022-06-13 23:34:45 +0800

    完备检测逻辑

--- impl.py
+++ impl.py
@@ -25,24 +25,49 @@ def point_in_polygon(point, polygon):
             logger.debug(f"第{index}个顶点[{x1}, {y1}]与测试点重合，在内部。")
             return True
 
-        if y1 == y2:
-            logger.debug(f"第{index}条边[[{x1}, {y1}], [{x2}, {y2}]]与射线平行，忽略。")
-            continue
-
         if y < min(y1, y2):
-            logger.debug(f"第{index}条边[[{x1}, {y1}], [{x2}, {y2}]]在射线上方，忽略。")
+            logger.debug(f"第{index}条边[[{x1}, {y1}], [{x2}, {y2}]]在延长线上方，忽略。")
             continue
 
-        if y >= max(y1, y2):
-            logger.debug(f"第{index}条边[[{x1}, {y1}], [{x2}, {y2}]]在射线下方，忽略。")
+        if y > max(y1, y2):
+            logger.debug(f"第{index}条边[[{x1}, {y1}], [{x2}, {y2}]]在延长线下方，忽略。")
             continue
 
+        if y1 == y2:
+            if y == y1:
+                if x > min(x1, x2) and x < max(x1, x2):
+                    logger.debug(
+                        f"点在第{index}条边[[{x1}, {y1}], [{x2}, {y2}]]上，属于内部。")
+                    return True
+                else:
+                    logger.debug(
+                        f"第{index}条边[[{x1}, {y1}], [{x2}, {y2}]]与延长线有重叠，但是点在边外部，忽略。")
+                continue
+
+        # 求交点
         _x = (x1 - x2) / (y1 - y2) * (y - y2) + x2
-        if _x > x:
-            logger.debug(
-                f"第{index}条边[[{x1}, {y1}], [{x2}, {y2}]]与射线存在交点[{_x}, {y}]，+1。")
+        if _x == x:
+            logger.debug(f"点在第{index}条边[[{x1}, {y1}], [{x2}, {y2}]]上，属于内部。")
+            return True
+
+        if _x < x:
+            logger.debug(f"第{index}条边[[{x1}, {y1}], [{x2}, {y2}]]与延长线无交点，忽略。")
+            continue
+
+        # 以下部分：_x > x
+        if y == min(y1, y2):
             intersection_count += 1
-        else:
-            logger.debug(f"第{index}条边[[{x1}, {y1}], [{x2}, {y2}]]与射线无交点，忽略。")
+            logger.debug(
+                f"第{index}条边[[{x1}, {y1}], [{x2}, {y2}]]上端与延长线相交[{_x}, {y}]，+1。")
+            continue
+
+        if y == max(y1, y2):
+            logger.debug(
+                f"第{index}条边[[{x1}, {y1}], [{x2}, {y2}]]下端与射线存在交点[{_x}, {y}]，但是需要忽略。")
+            continue
+
+        intersection_count += 1
+        logger.debug(
+            f"第{index}条边[[{x1}, {y1}], [{x2}, {y2}]]与射线存在交点[{_x}, {y}]，+1。")
 
     return True if intersection_count % 2 == 1 else False
